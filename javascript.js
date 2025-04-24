ce3033f999f2f79738b0fb77a3951cf29de5f735
Flavio <desenvolvedorbr37@gmail.com>
2025-04-24 18:17:53 -0300

    1817

--- javascript.js
+++ javascript.js
@@ -1,4 +1,25 @@
-$(' nav a').click(function(e){
-    e.preventDefault();
-   
-});
+const menuItems= document.querySelectorAll('.menu a');
+menuItems.forEach(item=>{
+   item.addEventListener('click',scrollToIdOnclick);
+})
+function scrollToIdOnclick(event){
+  event.preventDefault();
+ 
+ 
+  const to = getScrollTopByhref(event.target)-80;
+   scrollToPosition(to)
+}
+
+function scrollToPosition(to){
+  window.scroll({
+    top:to,
+    behavior:'smooth',
+  });
+
+}
+
+
+function getScrollTopByhref(element){
+  const id= element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
